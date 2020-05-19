fb1833bc27f1331f2220e8c318d9f55a0cd7aa89
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-18 18:25:50 -0700

    added files

---
+++ chapter_1_code/src/thermostat.java
@@ -0,0 +1,15 @@
+public class thermostat {
+
+    private float currentTemp;
+
+    private float desiredTemp;
+
+    public void furnace_on(){
+        // method body goes here
+    }
+
+    public void furnace_off(){
+        // method body goes here
+    }
+
+}
