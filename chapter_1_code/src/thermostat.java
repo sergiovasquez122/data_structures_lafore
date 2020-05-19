df9a94a1f7bfed930f1d344d8a332a4510ab8cc9
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-18 18:28:11 -0700

    added main

--- chapter_1_code/src/thermostat.java
+++ chapter_1_code/src/thermostat.java
@@ -12,4 +12,10 @@ public class thermostat {
         // method body goes here
     }
 
+    public static void main(String[] args) {
+        thermostat therm1, therm2; // create two references
+
+        therm1 = new thermostat(); // create two objects and stores references to them
+        therm2 = new thermostat();
+    }
 }
