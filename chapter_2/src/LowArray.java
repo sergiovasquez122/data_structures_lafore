62ce4284fffe1e0f078dc648608730ac54c41208
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-18 20:36:53 -0700

    added LowArray Class

---
+++ chapter_2/src/LowArray.java
@@ -0,0 +1,15 @@
+public class LowArray {
+    private long[] a;
+
+    public LowArray(int size){
+        a = new long[size];
+    }
+
+    public void setElem(int index, long value){
+        a[index] = value;
+    }
+
+    public long getElem(int index){
+        return a[index];
+    }
+}
