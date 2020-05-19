3410244dc8baca6dd141cd496b36468178bf0196
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-18 22:17:43 -0700

    completion of person app

---
+++ chapter_2/src/ClassDataApp.java
@@ -0,0 +1,40 @@
+public class ClassDataApp {
+    public static void main(String[] args) {
+        int max_size = 100;
+        ClassDataArray arr;
+        arr = new ClassDataArray(max_size);
+
+        arr = new ClassDataArray(max_size);
+
+        arr.insert("Evans", "Patty", 24);
+        arr.insert("Smith", "Lorraine", 37);
+        arr.insert("Yee", "Tom", 43);
+        arr.insert("Adams", "Henry", 63);
+        arr.insert("Hashimoto", "Sato", 21);
+        arr.insert("Stimson", "Henry", 29);
+        arr.insert("Velasquez", "Jose", 72);
+        arr.insert("Lamaarque", "Henry", 54);
+        arr.insert("Vang", "Minh", 22);
+        arr.insert("Creswell", "Lucinda", 18);
+        arr.displayA();
+
+        String search_key = "Stimson";
+        Person found;
+        found = arr.find(search_key);
+        if(found != null){
+            System.out.print("Found");
+            found.displayPerson();
+        } else {
+            System.out.println("Can't find " + search_key);
+        }
+
+        System.out.println("Deleting Smith, Yee, and Croswell");
+        arr.delete("Smith");
+        arr.delete("Yee");
+        arr.delete("Creswell");
+
+        arr.displayA();
+
+
+    }
+}
---
+++ chapter_2/src/ClassDataArray.java
@@ -0,0 +1,41 @@
+public class ClassDataArray {
+    private Person[] a;
+    private int nElems;
+
+    public ClassDataArray(int max){
+        a = new Person[max];
+        nElems = 0;
+    }
+
+    public Person find(String search_name){
+        int j;
+        for(j = 0;j < nElems;++j){
+            if(a[j].getLast().equals(search_name)) break;
+        }
+        if(j == nElems)
+            return null;
+        else return a[j];
+    }
+
+    public void insert(String last, String first, int age){
+        a[nElems] = new Person(last, first, age);
+        nElems++;
+    }
+
+    public boolean delete(String search_name){
+        int j;
+        for(j = 0;j < nElems;++j){
+            if(a[j].getLast().equals(search_name))break;
+        }
+        if(j == nElems) return false;
+        for(int k = j;k < nElems;++k)
+            a[k] = a[k + 1];
+        nElems--;
+        return true;
+    }
+
+    public void displayA(){
+        for(int j = 0;j < nElems;++j)
+            a[j].displayPerson();
+    }
+}
