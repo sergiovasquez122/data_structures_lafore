c9d13cef20e628ae4ee5ff7d88e3b5d291191c14
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-20 20:05:35 -0700

    added person class

---
+++ chapter_3/src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private String lastName;
+    private String firstName;
+    private int age;
+
+    public Person(String last, String first, int a){
+        lastName = last;
+        firstName = first;
+        age = a;
+    }
+
+    public void displayPerson(){
+        System.out.print(" Last Name:" + lastName);
+        System.out.print(" First Name:" + firstName);
+        System.out.println(" Age" + age);
+    }
+
+    public String getLast(){
+        return lastName;
+    }
+}
