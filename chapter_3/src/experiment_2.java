6d5eb7fd1fe29a1e862907deca8e94d35e2b2546
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-20 21:57:19 -0700

    added experiment_2

---
+++ chapter_3/src/experiment_2.java
@@ -0,0 +1,9 @@
+/**
+ * Insert date inversly sorted into an array using the same amount of data as in Experiment 1.
+ * See how fast the sorrt runs cmpared with the random data in experiment 1
+ */
+public class experiment_2 {
+    public static void main(String[] args) {
+
+    }
+}
