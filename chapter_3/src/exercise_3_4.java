01c0e8b15039e899c2d56886f7c088f71a7e1bfc
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-22 15:54:43 -0700

    completed exercise_3_4

--- chapter_3/src/exercise_3_4.java
+++ chapter_3/src/exercise_3_4.java
@@ -4,9 +4,21 @@
 public class exercise_3_4 {
     public static void main(String[] args) {
         int max_size = 20;
+        System.out.println("Sorting already sorted array");
         ArrayBub arrayBub = new ArrayBub(20);
         for(int i = 0;i < max_size; ++i){
             arrayBub.insert(i);
         }
+        arrayBub.oddEvenSort();
+        arrayBub.display();
+        arrayBub.clear();
+
+        System.out.println("Sorting reversely sorted array");
+        for(int i = max_size - 1;i >= 0;--i){
+            arrayBub.insert(i);
+        }
+        arrayBub.oddEvenSort();
+        arrayBub.display();
+        arrayBub.clear();
     }
 }
