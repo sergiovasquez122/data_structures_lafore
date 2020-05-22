63ccf5c356db502751ea290df602c31497715946
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-22 15:06:39 -0700

    partially completed exercise 3.5

--- chapter_3/src/exercise_3_5.java
+++ chapter_3/src/exercise_3_5.java
@@ -6,16 +6,22 @@ public class exercise_3_5 {
         int max_size = 20;
         ArrayIns arrayIns = new ArrayIns(20);
 
+        System.out.println("Already Sorted array");
         // inserts all items into sorted order
         for(int i = 0;i < max_size;++i){
             arrayIns.insert(i);
         }
+        arrayIns.insertionSortCompare();
+        arrayIns.clear();
+        System.out.println();
 
+        System.out.println("Reversely sorted array");
+        // inserts all items into sorted order
         // insert all items into unsorted order
         for(int i = max_size - 1;i >= 0;--i){
             arrayIns.insert(i);
         }
-
         arrayIns.insertionSortCompare();
+        System.out.println();
     }
 }
