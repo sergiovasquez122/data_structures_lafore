52df4ed39a84c40318512aa777677cc263bb119a
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-22 19:00:42 -0700

    completed bracketchecker

---
+++ chapter_4/src/BracketsApp.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BracketsApp {
+    public static void main(String[] args) {
+        while(true){
+            System.out.print("Enter string containing delimiters: ");
+            System.out.flush();
+            String input = getString();
+            if(input.equals(""))
+                break;
+
+            BracketChecker theChecker = new BracketChecker(input);
+            theChecker.check();
+        }
+    }
+
+    public static String getString(){
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+}
