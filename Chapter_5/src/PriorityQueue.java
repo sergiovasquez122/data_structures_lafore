dd0e6c780e3cbbc0baab7706749790f15a11f49a
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-23 22:40:56 -0700

    added exercise 5.1

---
+++ Chapter_5/src/PriorityQueue.java
@@ -0,0 +1,7 @@
+/**
+ * exercise 5.1
+ * Implement a priority queue based on a sorted linked list. The
+ * remove operation should remove the item with the smallest key
+ */
+public class PriorityQueue {
+}
