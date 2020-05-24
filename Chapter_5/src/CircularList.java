452c4ae94984633d99b0b353b9ed39afeead4003
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-23 23:18:57 -0700

    added exercise 5.4

---
+++ Chapter_5/src/CircularList.java
@@ -0,0 +1,5 @@
+/**
+ * Exercise 5.3 Implement a Circular linked list
+ */
+public class CircularList {
+}
---
+++ Chapter_5/src/Stack.java
@@ -0,0 +1,6 @@
+/**
+ * Exercise 5.4
+ * Implement a Stack Based on a Circular Linked list
+ */
+public class Stack {
+}
