f271debc04af86e35f7f550a1ffe28fd2a8400e7
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-24 10:42:36 -0700

    partial completion of matrix

--- Chapter_5/src/Matrix.java
+++ Chapter_5/src/Matrix.java
@@ -1,5 +1,34 @@
 /**
- * Exercise 5.6 Implement a matrix using a ilnked list
+ * Exercise 5.6 Implement a matrix using a linked list
  */
 public class Matrix {
+    private class Node{
+        private int value;
+        private Node bottomlink;
+        private Node rightlink;
+
+        private Node(){value = 0;}
+    }
+
+    private int rows;
+    private int cols;
+    Node upperleft;
+
+    public Matrix(int m, int n){
+    }
+
+    public void updateValue(int m, int n, int value){
+        Node current = upperleft;
+        for(int i = 0;i < m;++i) current = current.bottomlink;
+        for(int i = 0;i< n;++i) current = current.rightlink;
+        current.value = value;
+    }
+
+    public int rows(){
+        return rows;
+    }
+
+    public int cols(){
+        return cols;
+    }
 }
