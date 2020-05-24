b82e838323b3557c3d84f4c47a33c2f2cce10d71
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-24 09:31:57 -0700

    Added SortedList

---
+++ Chapter_5/src/SortedLIst.java
@@ -0,0 +1,16 @@
+public class SortedLIst {
+    private class Node{
+        private long data;
+        private Node next;
+
+        public Node(long data){
+            this.data = data;
+        }
+
+        public void displayLink(){
+            System.out.print(data + " ");
+        }
+    }
+
+
+}
