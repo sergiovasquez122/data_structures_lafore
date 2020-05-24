d55773dbdd3d30aa31fa0dd62d44a241074ed933
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-24 08:47:05 -0700

    added Queue class

--- Chapter_5/src/Queue.java
+++ Chapter_5/src/Queue.java
@@ -1,2 +1,33 @@
 public class Queue {
+    private Link first;
+    private Link last;
+    private int size;
+
+    public Queue(){
+        first = null;
+        last = null;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return first == null;
+    }
+
+    public void push(int intValue, double Value){
+        Link newNode = new Link(2, 2.99);
+        if(first == null){
+            first = last = newNode;
+        } else {
+            last.next = newNode;
+        }
+    }
+
+    public double delete(){
+        double value = first.dData;
+        first = first.next;
+        return value;
+    }
 }
