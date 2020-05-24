1635e049efa3246155b48f0b67e2f61decf4aa4f
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-23 21:19:04 -0700

    completed exercise 4.2

--- chapter_4/src/Deque.java
+++ chapter_4/src/Deque.java
@@ -9,27 +9,37 @@ public class Deque {
     private int deque_array[];
     private int n_elems;
     private int max_size;
-    private int front;
-    private int rear;
+    private int head;
+    private int tail;
 
     public Deque(int s){
-
+        n_elems = 0;
+        max_size = s;
+        deque_array = new int[max_size];
+        head = 0;
+        tail = deque_array.length - 1;
     }
 
     public void insertLeft(int value){
-
+        deque_array[tail--] = value;
+        if(tail == 0)  tail = deque_array.length -1;
+        n_elems++;
     }
 
     public void insertRight(int value){
-
+        deque_array[head++] = value;
+        if(head ==  deque_array.length) head = 0;
+        n_elems++;
     }
 
     public int removeLeft(){
-        throw new NotImplementedException();
+        if(head == deque_array.length - 1) head = -1;
+        return deque_array[++head];
     }
 
     public int removeRight(){
-        throw new NotImplementedException();
+        if(tail == 0) tail = deque_array.length - 1;
+        return deque_array[--tail];
     }
 
     public boolean isEmpty(){
