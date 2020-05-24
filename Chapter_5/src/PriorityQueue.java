ff4a3b5fbd9adb53691edac4bf25eda5eb540614
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-24 10:14:37 -0700

    Partial completion of PriorityQueue

--- Chapter_5/src/PriorityQueue.java
+++ Chapter_5/src/PriorityQueue.java
@@ -4,4 +4,38 @@
  * remove operation should remove the item with the smallest key
  */
 public class PriorityQueue {
+    private class Node {
+        private int data;
+        private Node next;
+    }
+
+    private Node head;
+    private int size;
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void insert(int value){
+        Node newNode = new Node();
+        newNode.data = value;
+
+        Node current = head;
+        Node previous = null;
+        while(current != null && value > current.data){
+            previous = current;
+            current = current.next;
+        }
+
+    }
+
+    public int delete(){
+        int value = head.data;
+        head = head.next;
+        return value;
+    }
 }
