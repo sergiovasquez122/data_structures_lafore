784cc4b575f4504d8ae46208a80cffba6b270bc2
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-23 19:38:29 -0700

    added prototype for exercise 4.2

---
+++ chapter_4/src/Deque.java
@@ -0,0 +1,30 @@
+/**
+ * Exercise 4.2 Create a double ended-queue that
+ * supports wrap-arounds
+ */
+public class Deque {
+
+    public void insertLeft(int value){
+
+    }
+
+    public void insertRight(int value){
+
+    }
+
+    public int removeLeft(int value){
+
+    }
+
+    public int removeRight(int value){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public boolean isFull(){
+
+    }
+}
