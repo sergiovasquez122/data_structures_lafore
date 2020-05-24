45f842f65a662daaa08ae1964b383f94d3c5bd7f
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-24 09:37:18 -0700

    Completed SortedList

--- Chapter_5/src/SortedLIst.java
+++ Chapter_5/src/SortedLIst.java
@@ -13,4 +13,43 @@ public class SortedLIst {
     }
 
 
+    private Node first;
+
+    public SortedLIst(){}
+
+    public boolean isEmpty(){
+        return first == null;
+    }
+
+    public void insert(long value){
+        Node newNode = new Node(value);
+        Node previous = null;
+        Node current = first;
+        while(current != null && value > current.data){
+            previous = current;
+            current = current.next;
+        }
+        if(previous == null){
+            first = newNode;
+        } else {
+            previous.next = newNode;
+        }
+        newNode.next = current;
+    }
+
+    public Node remove(){
+        Node temp = first;
+        first = temp.next;
+        return temp;
+    }
+
+    public void displayList(){
+        System.out.print("List (first -> last): ");
+        Node current = first;
+        while(current != null){
+            current.displayLink();
+            current = current.next;
+        }
+        System.out.println();
+    }
 }
