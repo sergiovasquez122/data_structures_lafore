c1dc021f442e9cbd41eb17424ba8221d4b392fd3
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-13 19:57:15 -0700

    added solution

--- Chapter_6/src/Anagram.java
+++ Chapter_6/src/Anagram.java
@@ -1,2 +1,22 @@
 public class Anagram {
+    static int size;
+    static int count;
+    static char[] arrChar = new char[100];
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void displayWord(){
+        if(count < 99)
+            System.out.print(" ");
+        if(count < 9)
+            System.out.print(" ");
+        System.out.print(++count + " ");
+        for(int j = 0;j < size;++j)
+            System.out.print(++count + " ");
+        System.out.print("   ");
+        if(count % 6 == 0)
+            System.out.println("");
+    }
 }
