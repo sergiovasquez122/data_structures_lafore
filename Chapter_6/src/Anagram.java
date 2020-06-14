e8c1326f3a740e9f91816094f0434d50e6450f57
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-13 20:02:49 -0700

    added solution

--- Chapter_6/src/Anagram.java
+++ Chapter_6/src/Anagram.java
@@ -1,10 +1,33 @@
+import edu.princeton.cs.algs4.StdIn;
+
 public class Anagram {
     static int size;
     static int count;
     static char[] arrChar = new char[100];
 
     public static void main(String[] args) {
+        System.out.print("Enter a word: " );
+        String input = StdIn.readLine();
+        size = input.length();
+        count = 0;
+        for(int j = 0;j < size;++j){
+            arrChar[j] = input.charAt(j);
+        }
+        doAnagram(size);
+    }
 
+    public static void doAnagram(int newSize){
+        if(newSize == 1){
+            return;
+        }
+
+        for(int j = 0;j < newSize;++j){
+            doAnagram(newSize - 1);
+            if(newSize == 2)
+                displayWord();
+            rotate(newSize
+            );
+        }
     }
 
     public static void rotate(int newSize){
