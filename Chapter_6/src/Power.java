5c42fc34809758e803aede562eee4098604895c1
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-14 20:57:25 -0700

    updated pow

--- Chapter_6/src/Power.java
+++ Chapter_6/src/Power.java
@@ -1,2 +1,14 @@
+import edu.princeton.cs.algs4.StdIn;
+
 public class Power {
+    public static void main(String[] args) {
+        System.out.print("Enter a number x: ");
+        int x = StdIn.readInt();
+        System.out.print("Enter a number y: ");
+        int y = StdIn.readInt();
+    }
+
+    public static int pow(int x, int y){
+        return 0;
+    }
 }
