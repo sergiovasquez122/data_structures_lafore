a1347360ce2d4c2199dfdea3fb4dddef6d1c73eb
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-17 14:45:31 -0700

    added solution

--- Chapter_6/src/Knapsack.java
+++ Chapter_6/src/Knapsack.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Knapsack {
@@ -8,9 +9,33 @@ public class Knapsack {
         knapsack.add(7);
         knapsack.add(6);
         knapsack.add(5);
+        System.out.println(solver(knapsack, 20));
     }
 
-    public void solver(){
+    public static ArrayList<Integer> solver(ArrayList<Integer> values, int target){
+        ArrayList<Integer> result = new ArrayList<>();
+        if(helper(result, values, target, 0)){
+            return result;
+        }
+        return null;
+    }
 
+    public static boolean helper(ArrayList<Integer> result, ArrayList<Integer> values, int target, int begin){
+        if(target == 0){
+            return true;
+        } else if(target < 0){
+            return false;
+        }
+        else {
+            for(int i = begin + 1;i < values.size();++i){
+                result.add(values.get(i));
+                if(helper(result, values, target - values.get(i), i)){
+                    return true;
+                }
+                result.remove(result.size() - 1);
+            }
+            return false;
+        }
     }
 }
+
