5599ee1b59b45965106ffe5ca7f50b560d7c35f8
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-25 15:38:26 -0700

    partial implementation

--- Chapter_8/src/Huffman.java
+++ Chapter_8/src/Huffman.java
@@ -1,7 +1,10 @@
+import edu.princeton.cs.algs4.MinPQ;
+
 /**
  * Construct a huffman tree
  */
 public class Huffman {
+    private static int R = 256;
     private static class Node implements Comparable<Node>{
         private char ch;
         private int freq;
@@ -13,6 +16,22 @@ public class Huffman {
             this.left = left;
             this.right = right;
         }
+
+        private static Node buildTrie(int[] freq){
+            MinPQ<Node> pq = new MinPQ<>();
+            for(char c = 0;c < R;c++){
+                if(freq[c] > 0){
+                    pq.insert(new Node(c, freq[c], null, null));
+                }
+            }
+            while(pq.size() > 1){
+                Node x = pq.delMin();
+                Node y = pq.delMin();
+                pq.insert(new Node('\0', x.freq + x.freq, x, y));
+            }
+            return pq.delMin();
+        }
+
 
         @Override
         public int compareTo(Node that) {
