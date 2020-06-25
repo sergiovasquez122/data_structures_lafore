f5db6d23b557cf1227afda56a31e7cb469cf2c7e
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-25 15:46:19 -0700

    partial implementation

--- Chapter_8/src/Huffman.java
+++ Chapter_8/src/Huffman.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.StdIn;
 
 /**
  * Construct a huffman tree
@@ -17,22 +18,6 @@ public class Huffman {
             this.right = right;
         }
 
-        private static Node buildTrie(int[] freq){
-            MinPQ<Node> pq = new MinPQ<>();
-            for(char c = 0;c < R;c++){
-                if(freq[c] > 0){
-                    pq.insert(new Node(c, freq[c], null, null));
-                }
-            }
-            while(pq.size() > 1){
-                Node x = pq.delMin();
-                Node y = pq.delMin();
-                pq.insert(new Node('\0', x.freq + y.freq, x, y));
-            }
-            return pq.delMin();
-        }
-
-
         @Override
         public int compareTo(Node that) {
             return freq - that.freq;
@@ -41,5 +26,26 @@ public class Huffman {
         public boolean isLeaf(){
             return left == null && right == null;
         }
+    }
+
+    private static Node buildTrie(int[] freq){
+        MinPQ<Node> pq = new MinPQ<>();
+        for(char c = 0;c < R;c++){
+            if(freq[c] > 0){
+                pq.insert(new Node(c, freq[c], null, null));
+            }
+        }
+        while(pq.size() > 1){
+            Node x = pq.delMin();
+            Node y = pq.delMin();
+            pq.insert(new Node('\0', x.freq + y.freq, x, y));
+        }
+        return pq.delMin();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter a message: ");
+        String s = StdIn.readString();
+
     }
 }
