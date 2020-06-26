f9798508f53bd93bb3a38acbe6e368b6f5d23aae
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-26 14:53:03 -0700

    partial implementation

--- chapter_12/src/BST.java
+++ chapter_12/src/BST.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+/**
+ * 12.4 implement a Max Priority Queue using a BST
+ */
+public class BST<Key extends Comparable<Key>>{
+    private Node root;
 
-public class BST {
+    private class Node{
+        private Key key;
+        private Node left, right;
+        int N;
+        Node(Key key, Node left, Node right, int N){
+            this.key = key;
+            this.left = left;
+            this.right = right;
+            this.N = N;
+        }
+    }
+
+    public int size(){
+        return size(root);
+    }
+
+    private int size(Node x){
+        if(x == null){
+            return 0;
+        }
+        return x.N;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void insert(Key key){
+
+    }
+
 }
