a93a3746e3fbb68c46a58431c75d9579eb99b3fe
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-26 13:44:47 -0700

    partial implementation of MaxPQ

--- chapter_12/src/MaxPQ.java
+++ chapter_12/src/MaxPQ.java
@@ -1,31 +1,31 @@
 public class MaxPQ<Key extends Comparable<Key>> {
-    Key[] pq;
-    MaxPQ(){
-
-    }
+    private Key[] pq;
+    private int N;
 
     MaxPQ(int max){
-
-    }
-
-    MaxPQ(Key[] a){
-
+        pq = (Key[]) new Comparable[N];
+        N = 0;
     }
 
     void insert(Key v){
-
+        pq[++N] = v;
+        swim(N);
     }
 
     Key delMax(){
-
+        Key max = pq[0];
+        exch(0, N--);
+        pq[N + 1] = null;
+        sink(0);
+        return max;
     }
 
     boolean isEmpty(){
-
+        return N == 0;
     }
 
     int size(){
-
+        return N;
     }
 
     private boolean less(int i, int j){
@@ -42,6 +42,16 @@ public class MaxPQ<Key extends Comparable<Key>> {
         while(k > 0 && less((k - 1)/ 2, k)){
             exch((k - 1) / 2, k);
             k = (k - 1) / 2;
+        }
+    }
+
+    private void sink(int k){
+        while(2 * k + 1 <= N){
+            int j = 2 * k + 1;
+            if(j < N && less(j, j + 1)) j++;
+            if(!less(k, j)) break;
+            exch(k, j);
+            k = j;
         }
     }
 }
