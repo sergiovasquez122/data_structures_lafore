1fd5a3bd530559e5f22c20f0e18aba7dc31c70f5
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-26 15:01:41 -0700

    partial implementation

--- chapter_12/src/BST.java
+++ chapter_12/src/BST.java
@@ -8,10 +8,8 @@ public class BST<Key extends Comparable<Key>>{
         private Key key;
         private Node left, right;
         int N;
-        Node(Key key, Node left, Node right, int N){
+        Node(Key key, int N){
             this.key = key;
-            this.left = left;
-            this.right = right;
             this.N = N;
         }
     }
@@ -32,7 +30,25 @@ public class BST<Key extends Comparable<Key>>{
     }
 
     public void insert(Key key){
-
+        root = insert(root, key);
     }
 
+    private Node insert(Node x, Key key){
+        if(x == null) return new Node(key, 1);
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) x.left = insert(x.left, key);
+        else if(cmp > 0) x.right = insert(x.right, key);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Key max(){
+        Node x = max(root);
+        return x != null ? x.key : null;
+    }
+
+    private Node max(Node x){
+        if(x.right != null) return max(x.right);
+        return x;
+    }
 }
