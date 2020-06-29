0a3d0fe434c15b90479f49ca6e2c8a9cc877d987
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-29 12:17:46 -0700

    partial implementation

--- chapter_12/src/TreeHeap.java
+++ chapter_12/src/TreeHeap.java
@@ -2,4 +2,27 @@
  * 12.5 Write a heap using a binary tree structure.
  */
 public class TreeHeap {
+    private Node root;
+    private int N;
+
+    int size(){
+        return N;
+    }
+
+    boolean isEmpty(){
+        return N == 0;
+    }
+
+    public void insert(int val){
+
+    }
+
+    public int deleteMax(){
+        return 0;
+    }
+
+    private class Node{
+        Node parent, left, right;
+        int val;
+    }
 }
