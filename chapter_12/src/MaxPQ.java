0e4d7bf9de9d50997a907d58cf377bb2aa80d8c5
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-29 11:03:22 -0700

    partial implementation

--- chapter_12/src/MaxPQ.java
+++ chapter_12/src/MaxPQ.java
@@ -5,21 +5,16 @@ public class MaxPQ<Key extends Comparable<Key>> {
     private int N;
 
     MaxPQ(int max){
-        pq = (Key[]) new Comparable[max];
+        pq = (Key[]) new Comparable[max + 1];
         N = 0;
     }
 
     void insert(Key v){
-        pq[N++] = v;
+        pq[++N] = v;
         swim(N);
     }
 
     Key delMax(){
-        Key max = pq[0];
-        exch(0, N--);
-        pq[N + 1] = null;
-        sink(0);
-        return max;
     }
 
     boolean isEmpty(){
@@ -41,35 +36,16 @@ public class MaxPQ<Key extends Comparable<Key>> {
     }
 
     private void swim(int k){
-        while(k > 0 && less((k - 1)/ 2, k)){
-            exch((k - 1) / 2, k);
-            k = (k - 1) / 2;
+        while(k > 1 && less(k / 2, k)){
+            exch(k / 2, k);
+            k = k / 2;
         }
     }
 
     private void sink(int k){
-        while(2 * k + 1 <= N){
-            int j = 2 * k + 1;
-            if(j < N && less(j, j + 1)) j++;
-            if(!less(k, j)) break;
-            exch(k, j);
-            k = j;
-        }
     }
 
     public static void main(String[] args) {
-        MaxPQ<Integer> pq = new MaxPQ<>(31);
-
-        pq.insert(70);
-        pq.insert(40);
-        pq.insert(60);
-        pq.insert(20);
-        pq.insert(60);
-        pq.insert(100);
-        pq.insert(80);
-        pq.insert(90);
-        pq.insert(10);
-        pq.insert(90);
 
     }
 
