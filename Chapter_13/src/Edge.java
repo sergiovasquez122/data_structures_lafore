af2c8226cc25a6cd350f91172ac5f08b6a1396dd
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-30 15:19:49 -0700

    added Edge class

---
+++ Chapter_13/src/Edge.java
@@ -0,0 +1,16 @@
+public class Edge {
+    private int x, y;
+
+    public Edge(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+}
--- Chapter_13/src/KnightTour.java
+++ Chapter_13/src/KnightTour.java
@@ -1,2 +1,17 @@
+import edu.princeton.cs.algs4.Queue;
+
+/**
+ * programming exercise 12.5 implement the knight tour
+ */
 public class KnightTour {
+    private boolean marked[][];
+    Queue<Edge> tour;
+    public KnightTour(int x, int y){
+        tour = new Queue<>();
+
+    }
+
+    private boolean tour(boolean marked[][], Queue<Edge> tour, Edge e){
+        return false;
+    }
 }
