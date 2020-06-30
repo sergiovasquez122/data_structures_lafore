18fa06e3627317367b0dad2444d72211a99c06d0
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-30 15:28:52 -0700

    added solution

--- Chapter_13/src/KnightTour.java
+++ Chapter_13/src/KnightTour.java
@@ -5,13 +5,39 @@ import edu.princeton.cs.algs4.Queue;
  */
 public class KnightTour {
     private boolean marked[][];
-    Queue<Edge> tour;
-    public KnightTour(int x, int y){
+    private Queue<Edge> tour;
+    private boolean tourExists;
+    public KnightTour(int N, int x, int y){
         tour = new Queue<>();
+        marked = new boolean[N][N];
+        tourExists = tour(marked, tour, new Edge(x, y));
+    }
 
+    public boolean tourExist(){
+        return tourExists;
     }
 
     private boolean tour(boolean marked[][], Queue<Edge> tour, Edge e){
+        if(!feasible(marked, e)) return false;
+        tour.enqueue(e);
+        if(tour.size() == marked.length * marked.length){
+            return true;
+        }
+        marked[e.getX()][e.getY()] = true;
+
+        int positions[][] = {{2, 1}, {2, -1}, {1, 2}, {1, -2}, {-2, 1}, {-2, -1}, {-1, 2}, {-1, -2}};
+        for(int[] pos : positions){
+            int new_x = pos[0] + e.getX();
+            int new_y = pos[1] + e.getY();
+            if(tour(marked, tour, new Edge(new_x, new_y))){
+                return true;
+            }
+        }
+        marked[e.getX()][e.getY()] = false;
         return false;
+    }
+
+    private boolean feasible(boolean marked[][], Edge e){
+        return e.getX() >= 0 && e.getX() < marked.length && e.getY() >= 0 && e.getY() < marked[e.getX()].length && !marked[e.getX()][e.getY()];
     }
 }
