1f4054e4063d915da66c43c51b0b6cffa821587c
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-01 15:55:46 -0700

    added methods

--- chapter_14/src/Graph.java
+++ chapter_14/src/Graph.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import edu.princeton.cs.algs4.Bag;
 
 public class Graph {
+    private double adjMat[][];
+    private final int V;
+    private int E;
+
+    public Graph(int V){
+        this.V = V;
+        adjMat = new double[V][V];
+
+        for(int i = 0;i < V;++i)
+            for(int j = 0;j < V;++j)
+                adjMat[i][j] = Double.POSITIVE_INFINITY;
+    }
+
+    public void addEdge(int v, int w, double weight){
+        adjMat[v][w] = weight;
+        adjMat[w][v] = weight;
+        E++;
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public double weight(int v, int w){
+        return adjMat[v][w];
+    }
+
+    public Iterable<Integer> edgeTo(int v){
+        Bag<Integer> bag = new Bag<>();
+        for(int i = 0;i < V;++i) {
+            if (adjMat[v][i] < Double.POSITIVE_INFINITY) {
+                bag.add(i);
+            }
+        }
+        return bag;
+    }
 }
--- chapter_14/src/TSP.java
+++ chapter_14/src/TSP.java
@@ -2,4 +2,7 @@
  * implement the traveling salesman problem
  */
 public class TSP {
+    private double distTo[][];
+
+
 }
