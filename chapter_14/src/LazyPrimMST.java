88bf3018e96329d68306ff6488528df3f655db31
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-01 12:16:56 -0700

    added weight method

--- chapter_14/src/LazyPrimMST.java
+++ chapter_14/src/LazyPrimMST.java
@@ -5,11 +5,13 @@ public class LazyPrimMST {
     private boolean[] marked;
     private Queue<Edge> mst;
     private MinPQ<Edge> pq;
+    private double weight;
 
     public LazyPrimMST(EdgeWeightedGraph G){
         marked = new boolean[G.V()];
         pq = new MinPQ<>();
         mst = new Queue<>();
+        weight = 0;
 
         visit(G, 0);
         while(!pq.isEmpty()){
@@ -17,6 +19,7 @@ public class LazyPrimMST {
             int v = e.either(), w = e.other(v);
             if(marked[v] && marked[w]) continue;
             mst.enqueue(e);
+            weight += e.weight();
             if(!marked[v]) visit(G, v);
             if(!marked[w]) visit(G, w);
         }
@@ -30,5 +33,9 @@ public class LazyPrimMST {
 
     public Iterable<Edge> edges(){
         return mst;
+    }
+
+    public double weight(){
+        return weight;
     }
 }
