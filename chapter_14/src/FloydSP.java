3b628c3ba206f7516c83d03a3e5154d21e50d08d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-02 11:10:01 -0700

    added methods

--- chapter_14/src/FloydSP.java
+++ chapter_14/src/FloydSP.java
@@ -2,4 +2,13 @@
  * Implement floyd-shortest path algorithm
  */
 public class FloydSP {
+    private double[][] distTo;
+
+    public FloydSP(Graph G){
+        distTo = new double[G.V()][G.V()];
+
+        for(int i = 0;i < G.V();++i)
+            for(int j = 0;j < G.V();++j)
+                distTo[i][j] = Double.POSITIVE_INFINITY;
+    }
 }
