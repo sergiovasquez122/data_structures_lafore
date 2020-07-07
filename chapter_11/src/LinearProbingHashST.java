d2e546eb5cfd0223ce673431a0f2525c71c7b023
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-07 15:46:16 -0700

    refactored

--- chapter_11/src/LinearProbingHashST.java
+++ chapter_11/src/LinearProbingHashST.java
@@ -12,9 +12,10 @@ public class LinearProbingHashST<Key, Value> {
         keys = (Key[]) new Object[M];
         values = (Value[]) new Object[M];
         N = 0;
+        this.M = M;
     }
 
-    public int hash(Key key){
+    private int hash(Key key){
         return (key.hashCode() & 0x7fffffff) % M;
     }
 
@@ -43,6 +44,9 @@ public class LinearProbingHashST<Key, Value> {
     }
 
     public Value get(Key key){
-
+        for(int i = hash(key);keys[i] != null;i = (i + 1) % M)
+            if(keys[i].equals(key))
+                return values[i];
+        return null;
     }
 }
