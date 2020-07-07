a858d556fb1c88e5080f1e23fb2fb014fccca340
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-07 16:31:31 -0700

    added method

--- chapter_11/src/QuadraticProbingHash.java
+++ chapter_11/src/QuadraticProbingHash.java
@@ -1,5 +1,32 @@
 /**
  * Implement a Hash table that uses quadratic probing
  */
-public class QuadraticProbingHash {
+public class QuadraticProbingHash<Key, Value> {
+    private int N;
+    private int M;
+    private Key[] keys;
+    private Value[] values;
+
+    public QuadraticProbingHash(){
+
+    }
+
+    public QuadraticProbingHash(int value){
+
+    }
+
+    public int hash(Key key){
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public void insert(Key key, Value value){
+
+    }
+
+    public boolean contains(Key key){
+        for(int i = hash(key);keys[i] != null;++i)
+            if(key.equals(keys[i]))
+                return true;
+        return false;
+    }
 }
