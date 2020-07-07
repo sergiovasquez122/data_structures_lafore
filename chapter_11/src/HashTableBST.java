67235a79b240943c74bd126557c4322e5565cb04
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-07 15:01:15 -0700

    added method

--- chapter_11/src/HashTableBST.java
+++ chapter_11/src/HashTableBST.java
@@ -5,5 +5,21 @@ import edu.princeton.cs.algs4.RedBlackBST;
  */
 public class HashTableBST {
     private RedBlackBST<Integer,Integer> table[];
+    private int M;
+    private int N;
+
+    public HashTableBST(){
+        this(16);
+    }
+
+    public HashTableBST(int M){
+        this.M = M;
+        this.N = 0;
+        table = (RedBlackBST<Integer, Integer>[]) new RedBlackBST[M];
+        for(int i = 0;i < M;++i)
+            table[i] = new RedBlackBST<>();
+    }
+
+
 
 }
