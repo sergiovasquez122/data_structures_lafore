7d476fc02b2e5c78a2420a4d47a93b204771f00d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-07 14:57:08 -0700

    added HashTableBST

---
+++ chapter_11/src/HashTableBST.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.algs4.RedBlackBST;
+
+/**
+ * Implement a separate-chaining hash-table using a balanced binary search tree
+ */
+public class HashTableBST {
+    private RedBlackBST<Integer,Integer> table[];
+
+}
