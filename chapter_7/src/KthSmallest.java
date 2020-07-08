46e28ee78e60a4bc7fa522c2692c27c7354978a7
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-08 16:14:17 -0700

    added KthSmallest problem

---
+++ chapter_7/src/KthSmallest.java
@@ -0,0 +1,10 @@
+/**
+ * Implement a program to find the kth smallest element
+ * in an array
+ */
+public class KthSmallest {
+
+    public static void main(String[] args) {
+
+    }
+}
--- chapter_7/src/MedianFinding.java
+++ chapter_7/src/MedianFinding.java
@@ -1,21 +1,54 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  * Use the partitioning algorithm
  * in order to compute the median of an array
  */
 public class MedianFinding {
 
-    private static int partition(int left, int right, int pivot){
-        return left - 1;
+    private static int partition(int arr[], int left, int right, int pivot_idx){
+        int pivot_value = arr[pivot_idx];
+        swap(arr, pivot_idx, right);
+        int new_pivot_idx = left;
+        for(int i = left;i < right;++i){
+            if(arr[i] < pivot_value){
+                swap(arr, i, new_pivot_idx++);
+            }
+        }
+        swap(arr, new_pivot_idx, right);
+        return new_pivot_idx;
     }
+
+    public static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
 
     public static int median(int arr[]){
         if(arr.length == 0)
             throw new IllegalArgumentException("Given an empty array");
 
-        return 0;
+        int left = 0, right = arr.length - 1;
+        while(left < right){
+            int pivot_idx = StdRandom.uniform(left, right + 1);
+            int new_pivot_idx = partition(arr, left, right, pivot_idx);
+            if(new_pivot_idx == arr.length / 2){
+                return arr[new_pivot_idx];
+            }
+            if(new_pivot_idx < arr.length / 2){
+                left = new_pivot_idx + 1;
+            } else {
+                right = new_pivot_idx - 1;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-
+        int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int value = MedianFinding.median(arr);
+        System.out.println(value);
     }
 }
