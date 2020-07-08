e5e3b19520b15c71baa1392624521087aa4130ae
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-07 17:58:21 -0700

    added DigitFoldingHash class

---
+++ chapter_11/src/DigitFoldingHash.java
@@ -0,0 +1,13 @@
+/**
+ * Using a LinearProbing hashtable, use digit-folding as
+ * a hash technique. you program should work for any array size
+ * and any key size
+ */
+public class DigitFoldingHash<Key, Value> {
+    private int N;
+    private int M;
+    private Key[] keys;
+    private Value[] values;
+
+
+}
