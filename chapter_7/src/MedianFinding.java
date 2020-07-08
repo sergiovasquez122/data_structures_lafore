e9b27ce59fc166212a605800b679ecb62761dc4e
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-08 15:52:18 -0700

    added partial solution

--- chapter_7/src/MedianFinding.java
+++ chapter_7/src/MedianFinding.java
@@ -4,6 +4,17 @@
  */
 public class MedianFinding {
 
+    private static int partition(int left, int right, int pivot){
+        return left - 1;
+    }
+
+    public static int median(int arr[]){
+        if(arr.length == 0)
+            throw new IllegalArgumentException("Given an empty array");
+
+        return 0;
+    }
+
     public static void main(String[] args) {
 
     }
