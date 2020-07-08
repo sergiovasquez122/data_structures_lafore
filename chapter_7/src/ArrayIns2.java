a1402e9cc06ac05f9da337e8fd24dc8f670d4dad
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-08 14:55:57 -0700

    added solution

--- chapter_7/src/ArrayIns2.java
+++ chapter_7/src/ArrayIns2.java
@@ -1,12 +1,21 @@
-import edu.princeton.cs.algs4.StdRandom;
-
+/**
+ * Programming exercise 7.2
+ * Modify the quickSort program to count the number
+ * of copies and comparisons it makes during a sort and then
+ * display the total. This program
+ * should duplicate the performance of the quickSort2 workshop applet
+ * so the copies and comparisons for inversely sorted data should agree
+ */
 public class ArrayIns2 {
     private long[] theArray;
     private int nElems;
+    private int comparisons, copies;
 
     public ArrayIns2(int max){
         theArray = new long[max];
         nElems = 0;
+        comparisons = 0;
+        copies = 0;
     }
 
     public void insert(long value){
@@ -38,8 +47,9 @@ public class ArrayIns2 {
 
     public long medianOf3(int left, int right){
         int center = (left + right) / 2;
-        if(theArray[left] > theArray[center])
+        if(theArray[left] > theArray[center]) {
             swap(left, center);
+        }
 
         if(theArray[left] > theArray[right])
             swap(left, right);
@@ -47,6 +57,7 @@ public class ArrayIns2 {
         if(theArray[center] > theArray[right])
             swap(center, right);
 
+        comparisons += 3;
         swap(center, right - 1);
         return theArray[right - 1];
 
@@ -56,6 +67,15 @@ public class ArrayIns2 {
         long temp = theArray[i];
         theArray[i] = theArray[j];
         theArray[j] = temp;
+        copies++;
+    }
+
+    public int comparisons(){
+        return comparisons;
+    }
+
+    public int copies(){
+        return copies;
     }
 
     public int partitionIt(int left, int right, long pivot){
@@ -63,15 +83,18 @@ public class ArrayIns2 {
         int rightPtr = right - 1;
 
         while(true){
-            while(theArray[++leftPtr] < pivot);
-
-            while(theArray[--rightPtr] > pivot);
-
+            while(lt(theArray[++leftPtr], pivot));
+            while(lt(pivot, theArray[--rightPtr]));
             if(leftPtr >= rightPtr) break;
             else swap(leftPtr, rightPtr);
         }
         swap(leftPtr, right - 1);
         return leftPtr;
+    }
+
+    public boolean lt(long lhs, long rhs){
+        comparisons++;
+        return lhs < rhs;
     }
 
     public void manualSort(int left, int right){
@@ -81,6 +104,7 @@ public class ArrayIns2 {
         if(size == 2){
             if(theArray[left] > theArray[right])
                 swap(left, right);
+            comparisons++;
         } else {
             if(theArray[left] > theArray[right - 1])
                 swap(left, right - 1);
@@ -88,18 +112,16 @@ public class ArrayIns2 {
                 swap(left, right);
             if(theArray[right - 1] > theArray[right])
                 swap(right - 1, right);
+            comparisons += 3;
         }
     }
 
     public static void main(String[] args) {
-        int maxSize = 16;
-        ArrayIns2 arr = new ArrayIns2(maxSize);
-        for(int i = 0;i < maxSize;++i){
-            long value =  StdRandom.uniform(100);
-            arr.insert(value);
-        }
-        arr.display();
-        arr.quickSort();
-        arr.display();
+        int maxSize = 12;
+        ArrayIns2 arrayIns2 = new ArrayIns2(maxSize);
+        for(int i = 0;i < maxSize;++i)
+            arrayIns2.insert(maxSize - i);
+        arrayIns2.quickSort();
+        System.out.println("# of comparisons: " + arrayIns2.comparisons + " # of swaps: " + arrayIns2.copies);
     }
 }
