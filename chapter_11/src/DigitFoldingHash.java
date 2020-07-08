ad4c23f8145b80c59822674af71dbe88f6bd75e6
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-07 18:04:01 -0700

    added resize

--- chapter_11/src/DigitFoldingHash.java
+++ chapter_11/src/DigitFoldingHash.java
@@ -9,5 +9,44 @@ public class DigitFoldingHash<Key, Value> {
     private Key[] keys;
     private Value[] values;
 
+    public DigitFoldingHash(){
+        this(16);
+    }
 
+    public DigitFoldingHash(int M){
+        this.N = 0;
+        this.M = M;
+        keys = (Key[]) new Object[M];
+        values = (Value[]) new Object[M];
+    }
+
+    public void resize(int cap){
+        DigitFoldingHash<Key, Value> temp = new DigitFoldingHash<>(cap);
+        for(int i = 0;i < M;++i)
+            if(keys[i] != null)
+                temp.insert(keys[i], values[i]);
+        keys = temp.keys;
+        values = temp.values;
+        M = temp.M;
+    }
+
+    public void insert(Key key, Value value){
+
+    }
+
+    public Value get(Key Key){
+
+    }
+
+    public void delete(Key key){
+
+    }
+
+    public boolean contains(Key key){
+
+    }
+
+    public int hash(Key key){
+
+    }
 }
