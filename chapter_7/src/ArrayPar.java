81ce1f68da73a9ec2c37766f1c19b941dbb5028e
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-08 12:32:19 -0700

    added partition method

--- chapter_7/src/ArrayPar.java
+++ chapter_7/src/ArrayPar.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 public class ArrayPar {
     private long[] theArray;
     private int nElems;
@@ -17,23 +19,49 @@ public class ArrayPar {
 
     public void display(){
         System.out.print("A=");
-        for(int j = 0;j < nElems;++j)
-            System.out.print(theArray[j] + " ");
+        for(int i = 0;i < nElems;++i)
+            System.out.print(theArray[i] + " ");
         System.out.println();
     }
 
-    public int partition(int left, int right, long pivot){
+    public int partitionIt(int left, int right, long pivot){
         int leftPtr = left - 1;
         int rightPtr = right + 1;
         while(true){
-            while(leftPtr < right && theArray[++leftPtr] < pivot);
+            while(leftPtr < rightPtr && theArray[++leftPtr] < pivot);
 
-            while(rightPtr > left && theArray[--rightPtr] > pivot);
+            while(rightPtr > leftPtr && theArray[--rightPtr] > pivot);
 
             if(leftPtr >= rightPtr) break;
-
+            else swap(leftPtr, rightPtr);
         }
         return leftPtr;
     }
 
+    public void swap(int a, int b){
+        long temp = theArray[a];
+        theArray[a] = theArray[b];
+        theArray[b] = temp;
+    }
+
+
+    public static void main(String[] args) {
+        int maxSize = 16;
+        ArrayPar arr = new ArrayPar(maxSize);
+
+        for(int i = 0;i < maxSize;++i){
+            long n = StdRandom.uniform(200);
+            arr.insert(n);
+        }
+        arr.display();
+
+        long pivot = 99;
+        System.out.print("Pivot is " + pivot);
+        int size = arr.size();
+
+        int partDex = arr.partitionIt(0, size - 1, pivot);
+
+        System.out.println(", Partition is at index " + partDex);
+        arr.display();
+    }
 }
