d89f07f300ced42f728807ab343816a062e69132
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-08 16:18:06 -0700

    added problem description

--- chapter_7/src/KthSmallest.java
+++ chapter_7/src/KthSmallest.java
@@ -4,6 +4,17 @@
  */
 public class KthSmallest {
 
+
+    /**
+     * Finds the kth smallest element in the array
+     * @param arr a non-empty integer array
+     * @param k a number between 1 and the size of the array
+     * @return the number that would be the kth smallest if the array was sorted
+     */
+    public static int kthSmallest(int arr[], int k){
+        return 0;
+    }
+
     public static void main(String[] args) {
 
     }
